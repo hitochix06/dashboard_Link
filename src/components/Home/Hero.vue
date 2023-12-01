6f79ee0a49a8029cb4660443af142f2ac45f30ab
hitochix06 <pachara.philibert@hotmail.fr>
2023-12-01 18:49:17 +0100

    Fix button click event in Hero.vue

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -21,8 +21,8 @@
             />
           </div>
           <div class="container mx-auto flex justify-center mt-10">
-            <!-- <button
-              @click="isOpen = true"
+            <button
+              @click="$router.push('/connexion')"
               type="button"
               class="text-white bg-[#43B7BE] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2"
             >
@@ -42,7 +42,7 @@
                 />
               </svg>
               <div class="uppercase">Ajouter un raccourci</div>
-            </button> -->
+            </button>
           </div>
         </div>
       </div>
